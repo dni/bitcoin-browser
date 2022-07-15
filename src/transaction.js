3ecc23f2876c3fb83757fc045184943367be494c
dni <dni.khr@gmail.com>
2022-07-16 00:38:50 +0200

    add better logging, fix, bugs, add segwit compatibiliy, better test data

--- src/transaction.js
+++ src/transaction.js
@@ -1,22 +1,51 @@
+import o from "./opcode";
 
 export class TransactionDecoder {
   constructor(rawtx) {
+    this.loglevel = 2;
+    this.loglevels = [ "INFO", "DEBUG", "VERBOSE" ];
     this.rawtx = rawtx;
     this.offset = 0;
+    this.vin_count = 0;
+    this.vout_count = 0;
+    this.segwit = false;
     this.tx = {
       vin: [],
       vout: [],
     };
     this.decode();
+    this.log(this.tx);
+
+  }
+  log(log, lvl = 0) {
+    if(this.loglevel >= lvl) {
+      if (lvl == 0) {
+        console.log(log);
+      } else {
+        console.log(this.loglevels[lvl], "::", log);
+      }
+    }
+  }
+  parsescript(script){
+    let scripttype = "pubkey";
+    return {
+      asm: script,
+      hex: this.hexfrombytes(script),
+      type: scripttype,
+      script: [o.OP_FALSE],
+    };
   }
   parsebytes(size) {
     let delta = size * 2;
     let range = this.rawtx.slice(this.offset, this.offset + delta);
     let binary = [];
+    let hex = "";
     for (let i = 0; i < range.length; i+=2) {
-      let hex = range[i]+range[i+1];
-      binary.push(parseInt(hex, 16));
+      let byte = range[i]+range[i+1];
+      hex += byte;
+      binary.push(parseInt(byte, 16));
     }
+    this.log(`parsed ${size} byte chunk @ offset ${this.offset} -> ${hex}`, 2);
     this.offset += delta;
     return binary;
   }
@@ -35,67 +64,95 @@ export class TransactionDecoder {
     if (peak == "fd") {
       // <= 0xffff 	fd1234 	Prefix with fd, and the next 2 bytes is the VarInt (in little-endian).
       let length = this.rawtx.slice(this.offset+2, this.offset+2+4);
+      this.log(`parsed 4 byte VarInt chunk @ offset ${this.offset}`, 2);
       this.offset += 2+4;
       return parseInt(length, 16);
     }
     if (peak == "fe") {
       // <= 0xffffffff 	fe12345678 	Prefix with fe, and the next 4 bytes is the VarInt (in little-endian).
       let length = this.rawtx.slice(this.offset+2, this.offset+2+8);
+      this.log(`parsed 8 byte VarInt chunk @ offset ${this.offset}`, 2);
       this.offset += 2+8;
       return parseInt(length, 16);
     }
     if (peak == "ff") {
       // <= 0xffffffffffffffff 	ff1234567890abcdef 	Prefix with ff, and the next 8 bytes is the VarInt (in little-endian).
       let length = this.rawtx.slice(this.offset+2, this.offset+2+16);
+      this.log(`parsed 16 byte VarInt chunk @ offset ${this.offset}`, 2);
       this.offset += 2+16;
       return parseInt(length, 16);
     }
-    // Varint = 6a = 106 bytes
+    if (peak == "00" && this.segwit === false) {
+      this.segwit = true;
+      this.log(`parsed 4 byte VarInt chunk @ offset ${this.offset}`, 2);
+      this.offset += 4;
+      this.log(`VarInt | flag: true`, 2);
+      return this.getlength();
+    }
+    this.log(`parsed 2 byte VarInt chunk @ offset ${this.offset} -> ${peak}`, 2);
     this.offset += 2;
     return parseInt(peak, 16);
   }
+
   decode() {
     this.tx.version = parseInt(this.parsebytes(4).reverse().join(""));
-    console.log(`4 bytes reversed | version: ${this.tx.version}`);
-    this.tx.vin_count = this.getlength();
-    console.log(`VarInt | vin_count: ${this.tx.vin_count}`);
-    for (let i = 0; i < this.tx.vin_count; i++) {
-      let input = {};
-      input.txid = this.parsebytes(32).reverse();
-      input.txid_hex = this.hexfrombytes(input.txid);
-      console.log(`32 bytes reversed | input ${i} txid: ${input.txid_hex}`, input.txid);
-      input.vout = parseInt(this.parsebytes(4).reverse().join());
-      console.log(`4 bytes reversed | input ${i} vout: ${parseInt(input.vout)}`);
-      let script_sig_size = this.getlength();
-      console.log(`VarInt | input ${i} script_sig_size: ${script_sig_size}`);
-      let script_sig = this.parsebytes(script_sig_size);
-      input.scriptSig = {
-        asm: script_sig,
-        hex: this.hexfrombytes(script_sig)
-      };
-      console.log(`${script_sig_size} bytes | input ${i} script_sig: ${input.scriptSig.hex}`, input.scriptSig.asm);
-      input.sequence = this.parsebytes(4).reverse();
-      input.sequence_hex = this.hexfrombytes(input.sequence);
-      input.sequence_number = this.numberfrombytes(input.sequence);
-      console.log(`4 bytes reversed | input ${i} sequence: ${input.sequence_hex}`, input.sequence);
-      this.tx.vin.push(input);
+    this.log(`4 bytes reversed | version: ${this.tx.version}`, 1);
+    this.vin_count = this.getlength();
+    this.log(`VarInt | vin_count: ${this.vin_count}`, 2);
+    if (this.vin_count > 0) {
+      for (let i = 0; i < 1; i++) {
+        let input = {};
+        input.txid = this.parsebytes(32).reverse();
+        input.txid_hex = this.hexfrombytes(input.txid);
+        this.log(`32 bytes reversed | input ${i} txid: ${input.txid_hex}`, 1);
+        input.vout = this.numberfrombytes(this.parsebytes(4).reverse());
+        this.log(`4 bytes reversed | input ${i} vout: ${input.vout}`,1);
+        let script_size = this.getlength();
+        this.log(`VarInt | input ${i} script_size: ${script_size}`,2);
+        let script = this.parsebytes(script_size);
+        input.scriptSig = this.parsescript(script);
+        this.log(`${script_size} bytes | input ${i} scriptSig: ${input.scriptSig.hex}`, 1);
+        input.sequence = this.parsebytes(4).reverse();
+        input.sequence_hex = this.hexfrombytes(input.sequence);
+        input.sequence_number = this.numberfrombytes(input.sequence);
+        this.log(`4 bytes reversed | input ${i} sequence: ${input.sequence_hex}`, 1);
+        this.tx.vin.push(input);
+      }
     }
 
-    this.tx.vout_count = this.getlength();
-    console.log(`VarInt | vout_count: ${this.tx.vout_count}`);
-    for (let i = 0; i < this.tx.vout_count; i++) {
-      let output = {};
-      let output_bin = this.parsebytes(8).reverse();
-      output.value = this.numberfrombytes(output_bin) / 100000000;
-      console.log(`8 bytes reversed | ouput ${i} value: ${output.value}`);
-      let script_pub_key_size = this.getlength();
-      console.log(`VarInt | output ${i} script_pub_key_size: ${script_pub_key_size}`);
-      output.script_pub_key = this.parsebytes(script_pub_key_size);
-      output.script_pub_key_hex = this.hexfrombytes(output.script_pub_key);
-      console.log(`${script_pub_key_size} bytes | output ${i} script_pub_key: ${output.script_pub_key_hex}`, output.script_pub_key);
-      this.tx.vout.push(output);
+    this.vout_count = this.getlength();
+    this.log(`VarInt | vout_count: ${this.vout_count}`, 2);
+    if (this.vout_count > 0 && this.vout_count < 0xffff) {
+      for (let i = 0; i < this.vout_count; i++) {
+        let output = {
+          n: i
+        };
+        let output_bin = this.parsebytes(8).reverse();
+        output.value = this.numberfrombytes(output_bin) / 100000000;
+        this.log(`8 bytes reversed | ouput ${i} value: ${output.value}`, 1);
+        let script_pub_key_size = this.getlength();
+        this.log(`VarInt | output ${i} script_pub_key_size: ${script_pub_key_size}`, 2);
+        let script = this.parsebytes(script_pub_key_size);
+        output.scriptPubKey = this.parsescript(script);
+        this.log(`${script_pub_key_size} bytes | output ${i} script_pub_key: ${output.script_pub_key_hex}`, 1);
+        this.tx.vout.push(output);
+      }
+    }
+    if (this.segwit && this.vin_count > 0) {
+        let input = this.tx.vin[0];
+        let witness_stack = this.getlength();
+        if (witness_stack > 0) {
+          input.txinwitness = [];
+          for (let i = 0; i < witness_stack; i++) {
+            let witness_length = this.getlength();
+            let witness = this.parsebytes(witness_length);
+            let witness_hex = this.hexfrombytes(witness);
+            input.txinwitness.push(witness_hex);
+            this.log(`${witness_length} bytes | input ${0} txwitness: ${witness_hex}`, 1);
+          }
+        }
     }
     this.tx.locktime = parseInt(this.parsebytes(4).reverse().join(""));
-    console.log(`4 bytes reversed | locktime: ${this.tx.locktime}`);
+    this.log(`4 bytes reversed | locktime: ${this.tx.locktime}`, 1);
   }
 }
--- test/transaction.test.js
+++ test/transaction.test.js
@@ -5,23 +5,32 @@ import { TransactionDecoder } from "../src/transaction";
 
 transactions.forEach((tx) => {
   let i = transactions.indexOf(tx);
-  let decoder = null;
-  describe(`Decode Transaction: #${i}`, () => {
+  // if (i < 1) {
+  //   return;
+  // }
+  let decoder = new TransactionDecoder(tx.raw);
+  describe(`Decode Transaction: #${i} :: ${tx.name}`, () => {
     it('should initialize', () => {
-      decoder = new TransactionDecoder(tx.raw);
       chai.expect(decoder).to.not.equal(null);
     });
 
     it(`decoder.tx.version should equal to ${tx.data.version}`, () => {
       chai.expect(decoder.tx.version).to.equal(tx.data.version);
     });
-    it(`decoder.tx.vin_count should equal to ${tx.data.vin.length}`, () => {
-      chai.expect(decoder.tx.vin_count).to.equal(tx.data.vin.length);
+    let has_segwit = "txinwitness" in tx.data.vin[0];
+    it(`decoder.tx.segwit should equal to ${has_segwit}`, () => {
+      chai.expect(decoder.segwit).to.equal(has_segwit);
     });
-    describe(`Decode Inputs: `, () => {
+    it(`decoder.vin_count should equal to ${tx.data.vin.length}`, () => {
+      chai.expect(decoder.vin_count).to.equal(tx.data.vin.length);
+    });
+    describe(`Decode ${decoder.vin_count} Inputs: `, () => {
       tx.data.vin.forEach((input) => {
         let j = tx.data.vin.indexOf(input);
         describe(`Input: #${j} `, () => {
+          it(`input should exist at index ${j}`, () => {
+            chai.expect(j in decoder.tx.vin).to.equal(true);
+          });
           it(`txid_hex should equal to ${input.txid}`, () => {
             chai.expect(decoder.tx.vin[j].txid_hex).to.equal(input.txid);
           });
@@ -40,15 +49,50 @@ transactions.forEach((tx) => {
         });
       });
     });
-    describe(`Decode Outputs: `, () => {
+    describe(`Decode ${decoder.vout_count} Outputs: `, () => {
       tx.data.vout.forEach((output) => {
         let j = tx.data.vout.indexOf(output);
         describe(`Output: #${j} `, () => {
+          it(`output should exist at index ${j}`, () => {
+            chai.expect(j in decoder.tx.vout).to.equal(true);
+          });
+          it(`output n should equal to ${output.n}`, () => {
+            chai.expect(decoder.tx.vout[j].n).to.equal(output.n);
+          });
           it(`value should equal to ${output.value}`, () => {
             chai.expect(decoder.tx.vout[j].value).to.equal(output.value);
           });
+          it(`scriptPubKey.hex should equal to ${output.scriptPubKey.hex}`, () => {
+            chai.expect(decoder.tx.vout[j].scriptPubKey.hex).to.equal(output.scriptPubKey.hex);
+          });
+          it(`scriptPubKey.type should equal to ${output.scriptPubKey.type}`, () => {
+            chai.expect(decoder.tx.vout[j].scriptPubKey.type).to.equal(output.scriptPubKey.type);
+          });
         });
       });
+    });
+
+    if (has_segwit) {
+      describe(`Decode Witnesses: `, () => {
+        tx.data.vin.forEach((input) => {
+          let j = tx.data.vin.indexOf(input);
+          describe(`Input Witness: #${j} `, () => {
+            it(`witness length should match to ${input.txinwitness.length}`, () => {
+              chai.expect(decoder.tx.vin[j].txinwitness.length).to.equal(input.txinwitness.length);
+            });
+            tx.data.vin[0].txinwitness.forEach((witness) => {
+              let j = tx.data.vin[0].txinwitness.indexOf(witness);
+              it(`witness hex should match to ${witness}`, () => {
+                chai.expect(decoder.tx.vin[0].txinwitness[j]).to.equal(witness);
+              });
+            });
+          });
+        });
+      });
+    }
+
+    it(`decoder.tx.locktime should equal to ${tx.data.locktime}`, () => {
+      chai.expect(decoder.tx.locktime).to.equal(tx.data.locktime);
     });
   });
 });
